fc377a3ad72d0bcdbb60164290e9cc25b10c7080
Jhaines1988 <josephthaines@gmail.com>
2021-12-20 15:33:42 -0700

    working on blob/state

--- client/src/components/lessVerboseRecorder/recorderHook.js
+++ client/src/components/lessVerboseRecorder/recorderHook.js
@@ -24,13 +24,13 @@ const RecordState = () => {
       const newChunk = [e.data];
 
       let blob = new Blob(newChunk, { type: 'audio/mp3' });
-      setBlob(blob)
+      setBlob((prevBlobs)=>[...prevBlobs,blob])
       setAudioURL((prevUrls) => [...prevUrls, URL.createObjectURL(blob)]);
     };
 
-    recorder.addEventListener('dataavailable', handleData);
+    // recorder.addEventListener('dataavailable', handleData);
 
-    return () =>  recorder.removeEventListener('dataavailable', handleData);
+    // return () =>  recorder.removeEventListener('dataavailable', handleData);
 
   }, [recorder, isRecording]);
 
@@ -56,15 +56,35 @@ const RecordState = () => {
   ];
 };
 
+
+
 const getRecorder = async function () {
   const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-
   let mediaRecorder = new MediaRecorder(stream);
 
+  mediaRecorder.onstop = e => {
+    var blob = new Blob(chunks, { "type": "audio/mp3" });
+    chunks = []
+    let audioURL = URL.createObjectURL(blob)
+    setAudioURL(audioURL);
+  }
 
+  mediaRecorder.ondataavailable = function (e) {
+    chunks.push(e.data)
+  }
 
   return mediaRecorder;
 };
+
+// const getRecorder = async function () {
+//   const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+//   let mediaRecorder = new MediaRecorder(stream);
+
+
+
+//   return mediaRecorder;
+// };
 
 export default RecordState;
 
@@ -82,3 +102,66 @@ export default RecordState;
 //     recorder.pause();
 //   }
 // }
+const RecordState = () => {
+  const [audioURL, setAudioURL] = useState([]);
+  const [isRecording, setIsRecording] = useState('');
+  const [recorder, setRecorder] = useState(null);
+
+  let chunks = []
+
+  useEffect(() => {
+    if (!recorder) {
+      if (isRecording === 'start') {
+        getRecorder().then(setRecorder, console.error);
+      }
+      return;
+    }
+    isRecording === 'start' && recorder.state === 'paused'
+      ? recorder.resume()
+      : isRecording === 'start'
+        ? recorder.start()
+        : isRecording === 'stop' ? recorder.stop() : recorder.pause()
+
+    // const handleData = (e) => {
+    //   setAudioURL(() => [URL.createObjectURL(e.data)]);
+    // };
+
+    // recorder.addEventListener('dataavailable', handleData);
+    // return () => recorder.removeEventListener('dataavailable', handleData);
+  }, [recorder, isRecording]);
+
+  const startRecording = () => {
+    setIsRecording('start');
+  };
+
+  const stopRecording = () => {
+    setIsRecording('stop');
+  };
+
+  const pauseRecording = () => {
+    setIsRecording('pause');
+  };
+
+  const getRecorder = async function () {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    let mediaRecorder = new MediaRecorder(stream);
+
+    mediaRecorder.onstop = e => {
+      var blob = new Blob(chunks, { "type": "audio/mp3" });
+      chunks = []
+      let audioURL = URL.createObjectURL(blob)
+      setAudioURL(audioURL);
+    }
+
+    mediaRecorder.ondataavailable = function (e) {
+      chunks.push(e.data)
+    }
+
+    return mediaRecorder;
+  };
+
+  return [audioURL, isRecording, startRecording, stopRecording, pauseRecording];
+};
+
+
+export default RecordState;
