0802519741d40bc57d0e98828931b72a80e493a9
Alexander Olvera <alexander.olvera943@gmail.com>
2021-12-22 14:51:06 -0600

    Created and added new UserContext for later use with auth

---
+++ client/src/context/UserContext.js
@@ -0,0 +1,3 @@
+import { createContext } from 'react'
+
+export default UserContext = createContext({ user: null, username: null })
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
+import UserContext from './context/UserContext.js';
 
 import App from './components/app.jsx'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <UserContext.Provider value={{ user: null, username: null }}>
+      <App />
+    </UserContext.Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
