dbee2e1fde65cff72a22d3b929c3d9a702b37c2d
Alexander Olvera <alexander.olvera943@gmail.com>
2021-12-22 16:31:10 -0600

    Added new user data hook and user data context for auth

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import UserContext from './context/UserContext.js';
+import useUserData from './hooks/useUserData.js';
 
 import App from './components/app.jsx'
 
+const userData = useUserData()
+
 ReactDOM.render(
   <BrowserRouter>
-    <UserContext.Provider value={{ user: null, username: null }}>
+    <UserContext.Provider value={userData}>
       <App />
     </UserContext.Provider>
   </BrowserRouter>,
