7ed7143bb39767f9af8af429316cf7a82b762393
alexanderolvera <alexander.olvera943@gmail.com>
2021-12-25 15:30:49 -0600

    Created inital form to create the users username

---
+++ client/src/components/UserForm.jsx
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from '@mui/material/Button'
+import TextField from "@mui/material/TextField";
+import SendIcon from '@mui/icons-material/Send'
+
+import { db } from '../lib/firebase.js';
+import { doc, setDoc} from 'firebase/firestore'
+
+export default function UserForm({ userId }) {
+
+  const navigate = useNavigate()
+  const usernameField = useRef()
+  const submitBttn = useRef()
+
+  const handleSubmit = () => {
+    try {
+      await setDoc(doc(db, 'users', userId), { username: usernameField.value })
+      navigate('/dashboard', { replace: true })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return(
+    <>
+      <TextField id="username-field" label="Username" variant="outlined" ref={usernameField}/>
+      <Button variant="contained" endIcon={<SendIcon />} onClick={handleSubmit} ref={submitBttn}>Submit</Button>
+    </>
+  )
+}
