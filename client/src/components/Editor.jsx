92119519db1434238a25ae032b65bc09806902e0
alexanderolvera <alexander.olvera943@gmail.com>
2021-12-31 16:28:18 -0600

    Created new modal for importing audio layers

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 import LayerPlayer from "./LayerPlayer";
+import Box from '@mui/material/Box';
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import ImportAduio from "./ImportAudio";
 
 export default function Editor() {
 
-  const [modalIsOpen, setModalIsOpen] = useState();
+  const [importModalState, setImportModalState ] = useState();
+  const [audioLayers, setAudioLayers] = useState([])
+
+  const handleClose = () => {
+    setImportModalState(false)
+  }
 
   return (
     <>
       <Typography variant="h3">Editor Component</Typography>
+      <Button variant="outlined" onClick={() => {setImportModalState(true)}}>Import Audio Layers</Button>
       <LayerPlayer />
+      <Modal open={importModalState} onClose={handleClose}>
+        <Box sx={{ width: 500, height: 300, backgroundColor: 'white'}}>
+          <ImportAduio userId={'gqEkSDrIsMhrP9HJIjqg7VcMpQ93'} currentList={audioLayers} setParentLayers={setAudioLayers} close={handleClose}/>
+        </Box>
+      </Modal>
     </>
   )
 }
---
+++ client/src/components/ImportAudio.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import { storage } from '../lib/firebase.js';
+import { ref, listAll } from "firebase/storage";
+import Alert from '@mui/material/Alert'
+import Button from '@mui/material/Button'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import { useSnackbar } from 'material-ui-snackbar-provider';
+
+export default function ImportAduio({ userId, currentList, setParentLayers, close }) {
+
+  const [audioLayerList, setAudioLayerList] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [checked, setChecked] = React.useState([]);
+  const snackbar = useSnackbar();
+
+  useEffect(() => {
+    const listRef = ref(storage, `audio/${userId}`);
+    listAll(listRef)
+      .then((res) => {
+        let items = res.items.map((itemRef, index) => {
+          // All the items under listRef.
+          let layerName = itemRef.name.split('.webm')[0]
+          for (var currentLayer of currentList) {
+            if (currentLayer.layerName === layerName) {
+              setChecked((prev) => [...prev, index])
+            }
+          }
+          return { layerName, ref: itemRef}
+        });
+        setAudioLayerList(items)
+        setLoading(false)
+      }).catch((error) => {
+        snackbar.showMessage(<Alert variant='error'>There was an error getting files.</Alert>)
+        setLoading(false)
+      });
+  }, [])
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  const handleSubmit = () => {
+    let submitList = []
+    checked.forEach((value) => {
+      submitList.push(audioLayerList[value])
+    })
+    setParentLayers(submitList)
+    snackbar.showMessage(<Alert variant='success'>{`Imported ${checked.length} item(s)`}</Alert>)
+    close()
+  }
+
+  return(
+    <>
+      {loading ?
+        <p>loading...</p>
+        :
+        audioLayerList ?
+          <>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+              {audioLayerList.map((item, index) => {
+                const labelId = `checkbox-list-label-${index}`;
+                const disabled = checked.length >= 4 && checked.indexOf(index) === -1
+                return(
+                  <ListItem key={index}>
+                    <ListItemButton role={undefined} onClick={handleToggle(index)} dense disabled={disabled}>
+                      <ListItemIcon>
+                        <Checkbox
+                          edge="start"
+                          checked={checked.indexOf(index) !== -1}
+                          tabIndex={-1}
+                          disableRipple
+                          inputProps={{ 'aria-labelledby': labelId }}
+                        />
+                      </ListItemIcon>
+                      <ListItemText id={labelId} primary={item.layerName} />
+                    </ListItemButton>
+                  </ListItem>
+                )
+              })}
+            </List>
+            <Button variant={'outlined'} onClick={handleSubmit}>Import</Button>
+          </>
+          :
+          <p>No items to show</p>
+      }
+    </>
+  )
+}
