5c25e41d015e23877ebe503f5cf5694cd00cd960
Alexander Olvera <alexander.olvera943@gmail.com>
2022-01-03 09:35:25 -0600

    Refactored to use dynamic trackId information

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 
 import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
-import Button from "@mui/material/Button";
 import Container from '@mui/material/Container';
 import Modal from "@mui/material/Modal";
-import Typography from "@mui/material/Typography";
 import { useSnackbar } from "material-ui-snackbar-provider";
 
 import LayerPlayer from "./LayerPlayer";
@@ -13,16 +12,17 @@ import ImportAduio from "./ImportAudio";
 import Recorder from './Recorder';
 
 import { getTrackData } from "../utils/database";
-import { getTrackUrls  } from "../utils/storage";
+import { getTrackUrls } from "../utils/storage";
+import UserContext from "../context/UserContext";
 
 export default function Editor() {
 
   const [importModalState, setImportModalState] = useState(false);
   const [recordingModalState, setRecordingModalState] = useState(false);
   const [audioLayers, setAudioLayers] = useState([])
-  const [trackId, setTrackId] = useState('ArtNIYCmtxFDhi5Dnyp9')
+  const userData = useContext(UserContext);
   const snackbar = useSnackbar();
-  // const [trackId, setTrackId] = useState(null)
+  const { trackId } = useParams();
 
   const handleImportClose = () => {
     setImportModalState(false)
@@ -39,7 +39,7 @@ export default function Editor() {
         return getTrackUrls(data)
       })
       .then(trackWithUrls => {
-        let layers =[]
+        let layers = []
         for (var layer in trackWithUrls.layers) {
           layers.push(trackWithUrls.layers[layer])
         }
@@ -59,26 +59,26 @@ export default function Editor() {
 
   return (
     <>
-    <Container sx={{
-          border: 1,
-          maxHeight: '90vh',
-          minHeight: {xs: '100%', md: '70%'},
-          width: {xs: '100%', md: '70%'},
-          display: 'grid',
-          gridTemplateColumns: {xs: '3fr 2fr', md: '1fr 6fr'},
-          gridTemplateRows: {xs: '5fr 1fr', md: '6fr 1fr'}
-        }}>
+      <Container sx={{
+        border: 1,
+        maxHeight: '90vh',
+        minHeight: { xs: '100%', md: '70%' },
+        width: { xs: '100%', md: '70%' },
+        display: 'grid',
+        gridTemplateColumns: { xs: '3fr 2fr', md: '1fr 6fr' },
+        gridTemplateRows: { xs: '5fr 1fr', md: '6fr 1fr' }
+      }}>
 
-        <LayerPlayer layers={audioLayers} userId={'gqEkSDrIsMhrP9HJIjqg7VcMpQ93'} trackId={trackId} recordingHandler={recordingHandler} importHandler={importHandler} />
+        <LayerPlayer layers={audioLayers} userId={userData?.user?.uid} trackId={trackId} recordingHandler={recordingHandler} importHandler={importHandler} />
 
         <Modal open={importModalState} onClose={handleImportClose}>
-          <Box sx={{ backgroundColor: 'white', top: 50, maxHeight: '100vh', overflow: 'auto'}}>
-            <ImportAduio userId={'gqEkSDrIsMhrP9HJIjqg7VcMpQ93'} currentList={audioLayers} setParentLayers={setAudioLayers} close={handleImportClose}/>
+          <Box sx={{ backgroundColor: 'white', top: 50, maxHeight: '100vh', overflow: 'auto' }}>
+            <ImportAduio userId={userData?.user?.uid} currentList={audioLayers} setParentLayers={setAudioLayers} close={handleImportClose} />
           </Box>
         </Modal>
         <Modal open={recordingModalState} onClose={handleRecorderClose} >
-          <Box sx={{ backgroundColor: 'white', margin: 'auto', top: 50,  }}>
-            <Recorder currentList={audioLayers} setAudioLayers={setAudioLayers}/>
+          <Box sx={{ backgroundColor: 'white', margin: 'auto', top: 50, }}>
+            <Recorder currentList={audioLayers} setAudioLayers={setAudioLayers} />
           </Box>
         </Modal>
       </Container>
