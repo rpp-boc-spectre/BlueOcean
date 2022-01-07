a6cda77e9dd22e79660c6f38450607212614be0d
Alexander Olvera <alexander.olvera943@gmail.com>
2022-01-07 17:23:37 -0600

    Added a new database util function to return all public tracks. Added a simple display for these tracks on the home page

--- client/src/utils/database.js
+++ client/src/utils/database.js
@@ -47,9 +47,26 @@ export function getAllTracks(userId) {
       let q = query(collRef, where('user', '==', userId))
       const querySnapshot = await getDocs(q);
       let data = []
-      console.log(querySnapshot)
       querySnapshot.forEach((doc) => {
-        console.log(doc)
+        let docData = doc.data()
+        docData.id = doc.id
+        data.push(docData)
+      })
+      resolve(data)
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+export function getAllPublicTracks() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let collRef = collection(db, 'tracks')
+      let q = query(collRef, where('meta.public', '==', true))
+      const querySnapshot = await getDocs(q);
+      let data = []
+      querySnapshot.forEach((doc) => {
         let docData = doc.data()
         docData.id = doc.id
         data.push(docData)
