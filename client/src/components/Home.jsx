e6f590ee75f6dc1533e6924a3519c5994e888986
Logan Michalicek <logan.michalicek@gmail.com>
2022-01-07 21:44:31 -0700

    Added HomeTrackBar

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,16 +3,25 @@ import { Link } from 'react-router-dom'
 import { getAllPublicTracks } from "../utils/database";
 import { Typography, Button, Alert, Grid, Card, CardContent, CardActionArea, Box } from '@mui/material'
 import { useSnackbar } from "material-ui-snackbar-provider";
+import HomeTrackBar from './HomeTrackBar.jsx';
 
 export default function Home() {
-
   const [tracks, setTracks] = useState([])
+  const [tagTypes, setTagTypes] = useState([])
   const snackbar = useSnackbar();
 
   const getPublicTracks = () => {
     getAllPublicTracks()
       .then((data) => {
         setTracks(data)
+        let tags = []
+        data.forEach(track => {
+          if (track.meta.tag !== undefined && !tags.includes(track.meta.tag)) {
+            tags.push(track.meta.tag);
+          }
+        });
+        console.log(data);
+        setTagTypes(tags);
       })
       .catch((error) => {
         console.log(error)
@@ -26,25 +35,16 @@ export default function Home() {
 
   return (
     <div>
-      <Typography variant='h3'>Tracks</Typography>
-      <Box sx={{ width: '100%', height: 400}}>
-        <Grid container spacing={2}>
-          {tracks.map((track, index) => <Grid item key={index}>
-            <Card sx={{ maxWidth: 300, height: 350 }}>
-              <CardActionArea>
-                <CardContent>
-                  <Typography variant="h6" component="div">
-                    {track.meta.trackName}
-                  </Typography>
-                  <Typography variant="p" component="div">
-                    {track.meta.tag}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>)}
-        </Grid>
-      </Box>
+      {tagTypes.map((tag, idx) => {
+        let tempTracks = [];
+        if (tag === "General") {
+          tempTracks = tracks.filter(track => track.meta.tag === "General" || track.meta.tag === undefined || track.meta.tag === "");
+        } else {
+          tempTracks = tracks.filter(track => track.meta.tag === tag);
+        }
+        tempTracks = tempTracks.slice(0, 5); // Only get the first 5 for now
+        return (<HomeTrackBar tracks={tempTracks} tag={tag} key={idx}/>);
+      })}
       <Link to='/dashboard'>
         <Button variant="contained">Dashboard</Button>
       </Link>
---
+++ client/src/components/HomeTrackBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Typography, Grid, Card, CardContent, CardActionArea, Box } from '@mui/material'
+import { useSnackbar } from "material-ui-snackbar-provider";
+
+export default function HomeTrackBar ({ tracks, tag }) {
+  return (
+    <>
+      <Typography variant='h4' mt={1}>Popular Tracks in {tag}</Typography>
+      <Box sx={{margin: 2}}>
+        <Grid container spacing={2}>
+          {tracks.map((track, index) => <Grid item key={index}>
+            <CardActionArea>
+              <Card sx={{ maxWidth: 300, height: 200, width: 300 }}>
+                <CardContent>
+                  <Typography variant="h6" component="div">
+                    {track.meta.trackName}
+                  </Typography>
+                  <Typography variant="p" component="div">
+                    {track.meta.tag || "General"}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </CardActionArea>
+          </Grid>)}
+        </Grid>
+      </Box>
+    </>
+  );
+}
