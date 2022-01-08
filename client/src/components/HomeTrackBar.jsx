a7c974ad5b2c78320eb81aaf491a5896221bb7bc
Logan Michalicek <logan.michalicek@gmail.com>
2022-01-08 12:39:48 -0700

    Added card routing to editor

--- client/src/components/HomeTrackBar.jsx
+++ client/src/components/HomeTrackBar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Typography, Grid, Card, CardContent, CardActionArea, Box } from '@mui/material'
 
 export default function HomeTrackBar ({ tracks, tag }) {
+  const navigate = useNavigate();
   return (
     <>
       <Typography variant='h4' mt={1}>Popular Tracks in {tag}</Typography>
       <Box sx={{margin: 2}}>
         <Grid container spacing={2}>
           {tracks.map((track, index) => <Grid item key={index}>
-            <CardActionArea>
+            <CardActionArea onClick={() => navigate('/edit/' + track.id)}>
               <Card sx={{ height: 200, width: 300 }}>
                 <CardContent>
                   <Typography variant="h6" component="div">
