71a8e5876a226651f3add514cdced677d7dd398f
Alexander Olvera <alexander.olvera943@gmail.com>
2022-01-11 16:32:19 -0600

    Replaced snackbar with toast in SignUp

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -2,15 +2,13 @@ import { Button, TextField, Typography, Alert } from "@mui/material";
 import React, { useRef, useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../lib/firebase'
-import { useSnackbar } from 'material-ui-snackbar-provider';
+import toast from 'react-hot-toast;'
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { db } from '../lib/firebase.js';
-import { doc, setDoc} from 'firebase/firestore'
+import { doc, setDoc } from 'firebase/firestore'
 
 
 export default function SignUp({ navigate }) {
-  let snackbar = useSnackbar();
-
   const formRef = useRef()
   const [formEmail, setFormEmail] = useState('')
   const [formPassword, setFormPassword] = useState('')
@@ -19,26 +17,25 @@ export default function SignUp({ navigate }) {
 
   const handleSubmit = () => {
     createUserWithEmailAndPassword(auth, formEmail, formPassword)
-    .then(userCredential => {
-      let user = userCredential.user
-      return setDoc(doc(db, 'users', user.uid), { username: formUsername })
-    })
-    .then(() => {
-      snackbar.showMessage(<Alert severity="success" sx={{ width: '100%' }}>User Created</Alert>)
-      navigate('/dashboard')
-    })
-    .catch(error => {
-      console.log(error)
-      snackbar.showMessage(<Alert severity="error" sx={{ width: '100%' }}>There was an error creating your account</Alert>)
-    })
+      .then(userCredential => {
+        let user = userCredential.user
+        return setDoc(doc(db, 'users', user.uid), { username: formUsername })
+      })
+      .then(() => {
+        toast.custom(<Alert variant="filled" severity="success">User Created</Alert>)
+        navigate('/dashboard')
+      })
+      .catch(error => {
+        toast.custom(<Alert variant="filled" severity="error">There was an error creating your account</Alert>)
+      })
   }
 
   ValidatorForm.addValidationRule('isPasswordMatch', (value) => {
     if (value !== formPassword) {
-        return false;
+      return false;
     }
     return true;
-});
+  });
 
   return (
     <>
@@ -46,7 +43,7 @@ export default function SignUp({ navigate }) {
       <ValidatorForm ref={formRef} onSubmit={handleSubmit}>
         <TextValidator
           label="Email"
-          onChange={e => {setFormEmail(e.target.value)}}
+          onChange={e => { setFormEmail(e.target.value) }}
           name="email"
           value={formEmail}
           validators={['required', 'isEmail']}
@@ -54,7 +51,7 @@ export default function SignUp({ navigate }) {
         />
         <TextValidator
           label="Username"
-          onChange={e => {setFormUsername(e.target.value)}}
+          onChange={e => { setFormUsername(e.target.value) }}
           name="username"
           value={formUsername}
           validators={['required']}
@@ -63,7 +60,7 @@ export default function SignUp({ navigate }) {
         <br />
         <TextValidator
           label="Password"
-          onChange={e => {setFormPassword(e.target.value)}}
+          onChange={e => { setFormPassword(e.target.value) }}
           name="password"
           type="password"
           validators={['required']}
@@ -72,14 +69,14 @@ export default function SignUp({ navigate }) {
         />
         <TextValidator
           label="Repeat password"
-          onChange={e => {setFormRePassword(e.target.value)}}
+          onChange={e => { setFormRePassword(e.target.value) }}
           name="repeatPassword"
           type="password"
           validators={['isPasswordMatch', 'required']}
           errorMessages={['password mismatch', 'this field is required']}
           value={formRePassword}
         />
-        <br/>
+        <br />
         <Button variant="contained" type="submit">Submit</Button>
       </ValidatorForm>
     </>
