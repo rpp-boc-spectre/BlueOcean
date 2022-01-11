78eedfd16b9dde5f9252fc112f6edf290460ef6d
Alexander Olvera <alexander.olvera943@gmail.com>
2022-01-11 16:07:34 -0600

    Was going to add toast to Dashboard but realized we don't use that logout button anymore. Removed logout function from dashboard. Removed unused mui references and snackbar/toast.

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,33 +1,15 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-
 import UserContext from "../context/UserContext.js";
-
 import TrackList from "./dashboardComponents/TrackList.jsx";
-
-import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Alert from '@mui/material/Alert';
-import { useSnackbar } from 'material-ui-snackbar-provider'
-import toast from 'react-hot-toast';
+
 
 export default function Dashboard() {
 
   const userData = useContext(UserContext)
   const navigate = useNavigate()
-  const snackbar = useSnackbar()
-
-  const handleSignOut = async () => {
-    try {
-      await signOut(auth)
-      snackbar.showMessage(<Alert severity="success" sx={{ width: '100%' }}>Successfully Signed Out</Alert>)
-      navigate('/');
-    } catch (error) {
-      console.log(error)
-      snackbar.showMessage(<Alert severity="error" sx={{ width: '100%' }}>{`There was an error signing you out :(`}</Alert>)
-    }
-  }
 
   return (
     <>
