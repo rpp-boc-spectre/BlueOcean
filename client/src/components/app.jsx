53f6880c9c09621f4ecb76f2f6f53b2eaebd62d2
Alexander Olvera <alexander.olvera943@gmail.com>
2022-01-11 16:37:46 -0600

    Removed snackbar from the app component

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import UserContext from '../context/UserContext.js';
 import useUserData from '../hooks/useUserData.js';
-import { SnackbarProvider } from 'material-ui-snackbar-provider'
 import { LayerStoreProvider } from "../context/LayerContext.js";
 import { initialState, layerTableReducer } from "../lib/layerTableReducer.js";
 import Paper from '@mui/material/Paper';
@@ -21,34 +20,32 @@ export default function App() {
   const userData = useUserData()
 
   return (
-    <SnackbarProvider SnackbarProps={{ autoHideDuration: 4000 }}>
-      <UserContext.Provider value={userData}>
-        <LayerStoreProvider initialState={initialState} reducer={layerTableReducer}>
-          <div className="App">
-            <Paper sx={{ bgcolor: 'info.main' }}>
-              <ResponsiveHeader />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path='login' element={<Entry />} />
-                <Route path='edit/:trackId' element={<Editor />} />
-                <Route path='edit' element={
-                  <Editor />
-                } />
-                <Route path='dashboard' element={
-                  <RequireAuth>
-                    <Dashboard />
-                  </RequireAuth>
-                } />
-                <Route path='*' element={<NotFound />} />
-              </Routes>
-              <Toaster
-                position="top-center"
-                reverseOrder={false}
-              />
-            </Paper>
-          </div>
-        </LayerStoreProvider>
-      </UserContext.Provider>
-    </SnackbarProvider >
+    <UserContext.Provider value={userData}>
+      <LayerStoreProvider initialState={initialState} reducer={layerTableReducer}>
+        <div className="App">
+          <Paper sx={{ bgcolor: 'info.main' }}>
+            <ResponsiveHeader />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path='login' element={<Entry />} />
+              <Route path='edit/:trackId' element={<Editor />} />
+              <Route path='edit' element={
+                <Editor />
+              } />
+              <Route path='dashboard' element={
+                <RequireAuth>
+                  <Dashboard />
+                </RequireAuth>
+              } />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+            <Toaster
+              position="top-center"
+              reverseOrder={false}
+            />
+          </Paper>
+        </div>
+      </LayerStoreProvider>
+    </UserContext.Provider>
   );
 }
